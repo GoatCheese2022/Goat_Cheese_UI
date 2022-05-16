7fe9b86d45dd856f907feba4ba878c663b4a9efd
Ashley Galindo <ashigalindo@gmail.com>
2022-05-16 17:32:31 -0500

    Allowing background image to resize beyond margins of window

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
 import { STATUS } from 'literals';
 
+<<<<<<< Updated upstream
 import {ProfileData} from 'types';
+=======
+// @ts-ignore
+import {AudioData, ProfileData, Topic} from 'types';
+>>>>>>> Stashed changes
 
 export const description = 'Name Pronunciation Tool';
 export const name = 'Hello! And welcome! to Goat Cheese';
